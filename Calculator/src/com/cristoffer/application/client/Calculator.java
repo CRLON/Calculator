f6461089d6c3d83bbd6844e455d00059b9362b26
CRLON <cristofferlonn@gmail.com>
2017-08-22 20:07:14 +0200

    ...

--- Calculator/src/com/cristoffer/application/client/Calculator.java
+++ Calculator/src/com/cristoffer/application/client/Calculator.java
@@ -1,146 +1,196 @@
 package com.cristoffer.application.client;
 
-import com.cristoffer.application.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
+
 public class Calculator implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private VerticalPanel calculatorPanel = new VerticalPanel();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private HorizontalPanel equationPanel, buttonRowOne, buttonRowTwo, buttonRowThree, buttonRowFour, buttonRowFive;
+	private TextBox operand1TextBox = new TextBox();
+	private TextBox operand2TextBox = new TextBox();
+	private TextBox equationBox = new TextBox();
+	private Button calculateButton = new Button("Calculate");
+	private MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
+	private SuggestBox operatorTextBox = new SuggestBox(oracle);
+	private FlexTable answerTable = new FlexTable();
+	private FlexTable calculatorTable = new FlexTable();
+	private String[][] calculatorButtons = {{"C", ",", "%", "/"}, {"7", "8", "9", "*"},{"4", "5", "6", "-"},
+			{"1", "2", "3", "+"}, {"0", "="}};
+//	private HashMap<CalculatorPanels, HorizontalPanel> panelMap = new HashMap<>();
 
 	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-
-	/**
-	 * This is the entry point method.
+	 * Entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		
+		equationPanel = new HorizontalPanel();
+//		buttonRowOne = new HorizontalPanel();
+//		buttonRowTwo = new HorizontalPanel();
+//		buttonRowThree = new HorizontalPanel();
+//		buttonRowFour = new HorizontalPanel();
+//		buttonRowFive = new HorizontalPanel();
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
+		answerTable.setText(0, 0, "Question");
+		answerTable.setText(0, 1, " ");
+		answerTable.setText(0, 2, "Answer");
+		
+		for (int i = 1; i < 10; i++)
+			answerTable.setText(i, 0, Integer.toString(i));
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 4; j++) {
+				if(i == 4) {
+					calculatorTable.setWidget(i, 0, new Button(calculatorButtons[i][0], handleClick()));
+					calculatorTable.setWidget(i, 1, new Button(calculatorButtons[i][1], handleClick()));
+					calculatorTable.getWidget(i, 0).setSize("100%", "80px");
+					calculatorTable.getWidget(i, 1).setSize("100%", "80px");
+					break;
 				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
+				else {
+					calculatorTable.setWidget(i, j, new Button(calculatorButtons[i][j], handleClick()));
+					calculatorTable.getWidget(i, j).setSize("80px", "80px");
 				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
 			}
 		}
+		//Sets the zero and equals buttons to span over 2 columns each
+		calculatorTable.getFlexCellFormatter().setColSpan(4, 0, 2);
+		calculatorTable.getFlexCellFormatter().setColSpan(4, 1, 3);
 
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		addPanel.add(operand1TextBox);
+		addPanel.add(operatorTextBox);
+		addPanel.add(operand2TextBox);
+		addPanel.add(calculateButton);
+		
+		addPanel.add(calculatorTable);
+
+
+		// TODO Assemble Main panel.
+		mainPanel.add(answerTable);
+		mainPanel.add(addPanel);
+		// TODO Associate the Main panel with the HTML host page.
+		RootPanel.get("calc").add(mainPanel);
+		// TODO Move cursor focus to the input box.
+		calculateButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				calculate();
+			}
+		});
+	}
+
+	private void calculate() {
+
+		final String operator = operatorTextBox.getText().trim();
+		calculateButton.setFocus(true);
+		if ((!isValidOperator(operator)) 
+				|| !isDouble(operand1TextBox.getText().trim()) || !isDouble(operand2TextBox.getText().trim())) {
+			Window.alert("You have entered a non valid binary operator or one of the operands is not an integer or a "
+					+ "double");
+			return;
+		}
+
+		double operand1 = Double.parseDouble(operand1TextBox.getText());
+		double operand2 = Double.parseDouble(operand2TextBox.getText());
+		double answer;
+		int row = answerTable.getRowCount();
+
+		// Multiplication
+		if (operator.equals("*")) {
+			answer = multiplication(operand1, operand2);
+//			Window.alert("The answer is: " + answer);
+		}
+
+		// Modulo
+		else if (operator.equals("%")) {
+			answer = operand1 % operand2;
+//			Window.alert("The answer is: " + answer);
+		}
+
+		// addition
+		else if (operator.equals("/")) {
+			answer = division(operand1, operand2);
+//			Window.alert("The answer is: " + answer);
+		}
+		
+		else {
+			answer = addition(operand1, operand2);
+		}
+		
+		if (row < 10) {
+		answerTable.setText(row, 0, Double.toString(operand1) + " " + operator + " " + Double.toString(operand2));
+		answerTable.setText(row, 1, Double.toString(answer));
+		}
+		else {
+			answerTable.setText(row, 0, Double.toString(operand1) + " " + operator + " " + Double.toString(operand2));
+			answerTable.setText(row, 1, " = ");
+			answerTable.setText(row, 2, Double.toString(answer));
+			answerTable.removeRow(1);
+		}
+
+	}
+
+	private boolean isValidOperator(String operator) {
+		if (operator.equals("*") || operator.equals("+") || operator.equals("-") ||
+				operator.equals("/") || operator.equals("%")) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	private double addition(double operandOne, double operandTwo) {
+		return operandOne + operandTwo;
+	}
+
+	private double subtraction(double operandOne, double operandTwo) {
+		return operandOne - operandTwo;
+	}
+
+	private double multiplication(double operandOne, double operandTwo) {
+		return operandOne * operandTwo;
+	}
+
+	private double division(double operandOne, double operandTwo) {
+		return operandOne / operandTwo;
+	}
+
+	private double modulus(double operandOne, double operandTwo) {
+		return operandOne % operandTwo;
+	}
+
+	private ClickHandler handleClick() {
+		return new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+
+		}
+	};
+}
+	
+	// Checks if a String could be seen as an integer
+	public boolean isDouble(String input) {
+		try {
+			Double.parseDouble(input);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 }
