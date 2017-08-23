9f70a1fedabea12f6305f3c853aff6487d80a73b
CRLON <cristofferlonn@gmail.com>
2017-08-23 11:06:14 +0200

    Separated code in to methods

--- Calculator/src/com/cristoffer/application/client/Calculator.java
+++ Calculator/src/com/cristoffer/application/client/Calculator.java
@@ -24,12 +24,10 @@ public class Calculator implements EntryPoint {
 	private HorizontalPanel addPanel = new HorizontalPanel();
 	private TextBox operand1TextBox = new TextBox();
 	private TextBox operand2TextBox = new TextBox();
-	private Button calculateButton = new Button("Calculate");
-	private MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
-	private SuggestBox operatorTextBox = new SuggestBox(oracle);
+	private TextBox operatorTextBox = new TextBox();
 	private FlexTable answerTable = new FlexTable();
 	private FlexTable calculatorTable = new FlexTable();
-	private String[][] calculatorButtons = {{"C", ",", "%", "/"}, {"7", "8", "9", "*"},{"4", "5", "6", "-"},
+	private String[][] calculatorButtons = {{"C", ".", "%", "/"}, {"7", "8", "9", "*"},{"4", "5", "6", "-"},
 			{"1", "2", "3", "+"}, {"0", "="}};
 //	private HashMap<CalculatorPanels, HorizontalPanel> panelMap = new HashMap<>();
 
@@ -37,20 +35,68 @@ public class Calculator implements EntryPoint {
 	 * Entry point method.
 	 */
 	public void onModuleLoad() {
-
-//		buttonRowOne = new HorizontalPanel();
-//		buttonRowTwo = new HorizontalPanel();
-//		buttonRowThree = new HorizontalPanel();
-//		buttonRowFour = new HorizontalPanel();
-//		buttonRowFive = new HorizontalPanel();
-
-		answerTable.setText(0, 0, "Question");
+		createButtons();
+		answerTable.setText(0, 0, "Equation");
 		answerTable.setText(0, 1, " ");
 		answerTable.setText(0, 2, "Answer");
 		
 		for (int i = 1; i < 10; i++)
-			answerTable.setText(i, 0, Integer.toString(i));
+			answerTable.setText(i, 0, "-");
 		
+		//Sets the zero and equals buttons to span over 2 columns each
+		calculatorTable.getFlexCellFormatter().setColSpan(4, 0, 2);
+		calculatorTable.getFlexCellFormatter().setColSpan(4, 1, 3);
+
+		addPanel.add(operand1TextBox);
+		addPanel.add(operatorTextBox);
+		addPanel.add(operand2TextBox);
+		addPanel.add(calculatorTable);
+		
+		// TODO Assemble Main panel.
+		mainPanel.add(answerTable);
+		mainPanel.add(addPanel);
+		// TODO Associate the Main panel with the HTML host page.
+		RootPanel.get("calc").add(mainPanel);
+		// TODO Move cursor focus to the input box.
+		operand1TextBox.setReadOnly(true);
+		operand2TextBox.setReadOnly(true);
+		operatorTextBox.setReadOnly(true);
+		operand1TextBox.setFocus(true);
+		operatorTextBox.setWidth("20px");
+	}
+
+	private void calculate() {
+
+		final String operator = operatorTextBox.getText().trim();
+		Calculations calc = new Calculations();
+		double answer;
+		if (!isDouble(operand1TextBox.getText().trim()) || !isDouble(operand2TextBox.getText().trim())) {
+			Window.alert("You have entered a non valid binary operator or one of the operands is not an integer or a "
+					+ "double");
+			return;
+		}
+		double operand1 = Double.parseDouble(operand1TextBox.getText());
+		double operand2 = Double.parseDouble(operand2TextBox.getText());
+		int answerTableRow = answerTable.getRowCount();
+		answer = calc.Calculate(operand1, operand2, operator);
+		updateAnswerTable(answerTableRow, operand1, operator, operand2, answer);
+
+	}
+	
+	private void updateAnswerTable(int row, Double operandOne, String operator, Double operandTwo, Double answer) {
+		if (row < 10) {
+		answerTable.setText(row, 0, Double.toString(operandOne) + " " + operator + " " + Double.toString(operandTwo));
+		answerTable.setText(row, 1, Double.toString(answer));
+		}
+		else {
+			answerTable.setText(row, 0, Double.toString(operandOne) + " " + operator + " " + Double.toString(operandTwo));
+			answerTable.setText(row, 1, " = ");
+			answerTable.setText(row, 2, Double.toString(answer));
+			answerTable.removeRow(1);
+		}
+	}
+	
+	private void createButtons() {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 4; j++) {
 				if(i == 4) {
@@ -66,62 +112,9 @@ public class Calculator implements EntryPoint {
 				}
 			}
 		}
-		//Sets the zero and equals buttons to span over 2 columns each
-		calculatorTable.getFlexCellFormatter().setColSpan(4, 0, 2);
-		calculatorTable.getFlexCellFormatter().setColSpan(4, 1, 3);
-
-		addPanel.add(operand1TextBox);
-		addPanel.add(operatorTextBox);
-		addPanel.add(operand2TextBox);
-		addPanel.add(calculateButton);
-		
-		addPanel.add(calculatorTable);
-
-
-		// TODO Assemble Main panel.
-		mainPanel.add(answerTable);
-		mainPanel.add(addPanel);
-		// TODO Associate the Main panel with the HTML host page.
-		RootPanel.get("calc").add(mainPanel);
-		// TODO Move cursor focus to the input box.
-		calculateButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				calculate();
-			}
-		});
 	}
 
-	private void calculate() {
-
-		final String operator = operatorTextBox.getText().trim();
-		Calculations calc = new Calculations();
-		double answer;
-		calculateButton.setFocus(true);
-		if ((!isValidOperator(operator)) 
-				|| !isDouble(operand1TextBox.getText().trim()) || !isDouble(operand2TextBox.getText().trim())) {
-			Window.alert("You have entered a non valid binary operator or one of the operands is not an integer or a "
-					+ "double");
-			return;
-		}
-		double operand1 = Double.parseDouble(operand1TextBox.getText());
-		double operand2 = Double.parseDouble(operand2TextBox.getText());
-		int row = answerTable.getRowCount();
-		answer = calc.Calculate(operand1, operand2, operator);
-
-		if (row < 10) {
-		answerTable.setText(row, 0, Double.toString(operand1) + " " + operator + " " + Double.toString(operand2));
-		answerTable.setText(row, 1, Double.toString(answer));
-		}
-		else {
-			answerTable.setText(row, 0, Double.toString(operand1) + " " + operator + " " + Double.toString(operand2));
-			answerTable.setText(row, 1, " = ");
-			answerTable.setText(row, 2, Double.toString(answer));
-			answerTable.removeRow(1);
-		}
-
-	}
-
-	private boolean isValidOperator(String operator) {
+	private boolean isOperator(String operator) {
 		if (operator.equals("*") || operator.equals("+") || operator.equals("-") ||
 				operator.equals("/") || operator.equals("%")) {
 			return true;
@@ -130,18 +123,43 @@ public class Calculator implements EntryPoint {
 			return false;
 		}
 	}
+	
+	private void clearCalc() {
+		operand2TextBox.setText("");
+		operand1TextBox.setText("");
+		operatorTextBox.setText("");
+		operand1TextBox.setFocus(true);
+	}
 
 	private ClickHandler handleClick() {
 		return new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent event) {
-
+				Button btn = (Button)event.getSource();
+				String buttonText = btn.getText();
+				if (buttonText.equals("C")) {
+					clearCalc();
+				}
+				else if (buttonText.equals("=")) {
+					calculate();
+					clearCalc();
+				}
+				else if(isOperator(buttonText) && !isOperator(operatorTextBox.getText())) {
+					operatorTextBox.setText(buttonText);
+					operand2TextBox.setFocus(true);
+				}
+				else if (!isOperator(buttonText) && !isOperator(operatorTextBox.getText())) {
+					operand1TextBox.setText(operand1TextBox.getText() + buttonText);
+				}
+				else if (!isOperator(buttonText) && isOperator(operatorTextBox.getText())) {
+					operand2TextBox.setText(operand2TextBox.getText() + buttonText);
+				}
 		}
 	};
 }
 	
-	// Checks if a String could be seen as an integer
+	// Checks if a String could be seen as a double
 	public boolean isDouble(String input) {
 		try {
 			Double.parseDouble(input);
